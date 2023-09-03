4fcf69f8fc9497c738cd02e72f10d65f87490b2a
Randhomme <50877413+Randhomme@users.noreply.github.com>
2023-09-03 19:04:01 +0200

    Update Form1.Designer.cs
    Merged Gun and Bullet items of the mesh scene type combo box.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -458,8 +458,7 @@
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Items.AddRange(new object[] {
             "Ship/Island/Planet/Animal",
-            "Gun",
-            "Bullet"});
+            "Gun/Bullet"});
             this.comboBox1.Location = new System.Drawing.Point(222, 8);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(722, 21);
