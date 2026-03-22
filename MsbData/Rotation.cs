cb3ea05313e482f5f19d5030a57b1ee36c3e6999
Randhomme <randormi@gmail.com>
2026-03-22 22:16:18 +0100

    feat: add rotation class

---
+++ MsbData/Rotation.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+
+namespace TPShipToolkit.MsbData
+{
+    [TypeConverter(typeof(ExpandableObjectConverter))]
+    public class Rotation
+    {
+        [DisplayName("1 - Yaw")]
+        [Description("Angle in degree")]
+        public float Yaw { get; set; }
+        [DisplayName("2 - Pitch")]
+        [Description("Angle in degree")]
+        public float Pitch { get; set; }
+        [DisplayName("3 - Roll")]
+        [Description("Angle in degree")]
+        public float Roll { get; set; }
+
+        public Rotation()
+        {
+            Yaw = 0;
+            Pitch = 0;
+            Roll = 0;
+        }
+
+        public override string ToString()
+        {
+            return Yaw + "; " + Pitch + "; " + Roll;
+        }
+    }
+}
