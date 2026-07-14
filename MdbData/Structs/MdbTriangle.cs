0a5d4133da1a1d23fe76e6d1d4e385fc290dde3c
Randhomme <randormi@gmail.com>
2026-07-14 12:46:33 +0200

    feat: add mdb structs

---
+++ MdbData/Structs/MdbTriangle.cs
@@ -0,0 +1,18 @@
+﻿namespace TPShipToolkit.MdbData.Structs
+{
+    public struct MdbTriangle
+    {
+        public ushort P0;
+        public ushort P1;
+        public ushort P2;
+        public ushort TextureIndex;
+
+        public MdbTriangle(ushort p0, ushort p1, ushort p2, ushort textureIndex)
+        {
+            P0 = p0;
+            P1 = p1;
+            P2 = p2;
+            TextureIndex = textureIndex;
+        }
+    }
+}
---
+++ MdbData/Structs/MdbVertex.cs
@@ -0,0 +1,25 @@
+﻿namespace TPShipToolkit.MdbData.Structs
+{
+    public struct MdbVertex
+    {
+        public float X, Y, Z;
+        public float U, V;
+        public float NX, NY;
+        public byte R, G, B, A;
+
+        public MdbVertex(float x, float y, float z, float u, float v, float nx, float ny, byte r, byte g, byte b, byte a)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+            U = u;
+            V = v;
+            NX = nx;
+            NY = ny;
+            R = r;
+            G = g;
+            B = b;
+            A = a;
+        }
+    }
+}
